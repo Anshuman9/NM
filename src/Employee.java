2778a2605e5ec6d8dcd05e4eecbcafde10ce10c4
Anshumman <Anshumman@Anshuma>
2018-05-10 16:06:39 +0530

    yessssss

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,11 @@
 
+public class Employee {
+
+	public static void main(String[] args) {
+	 int eid=101;
+		System.out.println("eid:+value");
+		System.out.println("eid");
+		System.out.println("eeiidd");
+	}
+
+}
