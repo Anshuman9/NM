a6c98f348c6fba835264016446ee85fd0aee830d
Anshumman <Anshumman@Anshuma>
2018-05-10 16:16:35 +0530

    remove

--- src/Employee.java
+++ src/Employee.java
@@ -2,10 +2,10 @@
 public class Employee {
 
 	public static void main(String[] args) {
-	 int eid=101;
+	 int eid=102;
 		System.out.println("eid:+value");
 		System.out.println("eid");
-		System.out.println("eeiidd");
+		
 	}
 
 }
